ebc0e2a47048a7bc59972ca25616b1ac134b3504
RoryHemmings <rpmhemmings@gmail.com>
2021-01-26 15:09:29 -0800

    Fixed gitignore

--- proof-of-concept/utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "utils.h"
-
-std::string colorText(const std::string& message)
-{
-  std::string ret = "\033[31m" + message + "\033[0m";
-  return ret;
-}
--- proof-of-concept/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef SFTP_POC_UTILS_H
-#define SFTP_POC_UTILS_H
-
-#include <string>
-
-std::string colorText(const std::string&);
-
-#endif
-
